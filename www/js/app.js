0887c642566ae772d8929b6fd899f6c659599496
hxx <yellowstar520@gmail.com>
2015-08-19 17:02:30 +0800

    change app from starter to rubyChina

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers'])
+angular.module('rubyChina', ['ionic', 'ionic-material', 'rubyChina.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -71,3 +65,5 @@ angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
+
+var rubyChina = angular.module('rubyChina');
