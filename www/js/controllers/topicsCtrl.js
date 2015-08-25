d6fff19e507bdcc9be67f6999a41dded118326a7
hxx <yellowstar520@gmail.com>
2015-08-25 17:32:18 +0800

    add pull refresh and infinite scroll

--- www/js/controllers/topicsCtrl.js
+++ www/js/controllers/topicsCtrl.js
@@ -2,27 +2,87 @@ rubyChina.controller('topicsCtrl',
 [
   '$scope',
   '$state',
+  '$timeout',
   '$ionicLoading',
   '$ionicPopup',
   '$cordovaToast',
   'Topics',
-  function($scope, $state, $ionicLoading, $ionicPopup, $cordovaToast, Topics) {
+  function($scope, $state, $timeout, $ionicLoading, $ionicPopup, $cordovaToast, Topics) {
     $ionicLoading.show({
       template: 'Loading...'
     });
 
-    Topics.get().$promise.then(
-      function(value) {
-        $ionicLoading.hide();
-        $scope.topics = value["topics"];
-        window.localStorage["topics"] = JSON.stringify($scope.topics);
-      },
-      function(error) {
-        $ionicLoading.hide();
-        $scope.topics = JSON.parse(window.localStorage["topics"]);
-        $ionicPopup.alert({
-          title: "网络连接发生错误",
-        });
+    $scope.offset = 0;
+    $scope.topics = [];
+
+    $scope.loadMoreTopics = function() {
+      Topics.get(
+        {
+          offset: $scope.offset,
+          limit: 10,
+        }
+      )
+      .$promise.then(
+        function(value) {
+          $ionicLoading.hide();
+          $scope.topics.push.apply($scope.topics, value["topics"]);
+          window.localStorage["topics"] = JSON.stringify($scope.topics);
+          $scope.moreTopics = value["topics"];
+          $scope.offset += 10;
+          $scope.$broadcast('scroll.infiniteScrollComplete')
+        },
+        function(error) {
+          $ionicLoading.hide();
+          $scope.topics = JSON.parse(window.localStorage["topics"]);
+          $ionicPopup.alert({
+            title: "网络连接发生错误",
+          });
+        }
+      );
+    }
+
+    $scope.loadMoreTopics();
+
+    $scope.moreTopicsCanBeLoaded = function() {
+      if ($scope.moreTopics && $scope.moreTopics.length != 0) {
+        return true;
       }
-    )
-}])
+      else {
+        return false;
+      }
+    }
+
+    // pull to refresh
+    $scope.refreshTopics = function() {
+      $timeout(function() {
+        $scope.offset = 0;
+        $scope.topics = [];
+
+        Topics.get(
+          {
+            offset: $scope.offset,
+            limit: 10,
+            timeout: 3000
+          }
+        )
+        .$promise.then(
+          function(value) {
+            $scope.topics.push.apply($scope.topics, value["topics"]);
+            window.localStorage["topics"] = JSON.stringify($scope.topics);
+            $scope.moreTopics = value["topics"];
+            $scope.offset += 10;
+          },
+          function(error) {
+            $scope.topics = JSON.parse(window.localStorage["topics"]);
+            $ionicPopup.alert({
+              title: "网络连接发生错误",
+            });
+          }
+        )
+        .finally(function() {
+          $scope.$broadcast('scroll.refreshComplete');
+        })
+      }, 1000);
+    };
+  }
+])
